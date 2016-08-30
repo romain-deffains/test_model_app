ac2db356b6bc4e705751d74a9364342b29612ad4
LzDowwind <romain.deffains@yahoo.fr>
2016-08-30 10:23:31 +0200

    Changement test.sh

--- test.sh
+++ test.sh
@@ -11,19 +11,15 @@ do
     do
 	if [ $letter = '*' ]
 	then
-	    echo "J'ai trouvé *.\n"
 	    $checkout=1
 	fi
     done
     if [ $checkout = 0 ]
     then
-	echo "Commande git.\n"
 	git checkout $line
 	git pull origin master
 	git add .
 	git commit -m "Rebase branch"
-	send "$user"
-	send "$pass"
 	git push origin $line
 	git checkout master
     fi
